d4308426db62f6119a43d043829009b1b0e96885
harishpolagani <39367904+harishpolagani@users.noreply.github.com>
2018-12-12 01:36:44 +0530

    Create linkedlist5.c

---
+++ linkedlist5.c
@@ -0,0 +1,90 @@
+//Find Length of a Linked List (Iterative)
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+
+void push(struct node **,int data);
+int countList(struct node *);
+void deleteList(struct node **);
+void printList(struct node *);
+
+int main()
+{
+    struct node *head = NULL;
+    push(&head,1);
+    push(&head,2);
+    push(&head,3);
+    push(&head,4);
+    push(&head,5);
+    push(&head,6);
+    printList(head);
+    
+    printf("count=%d \n", countList(head));
+
+    deleteList(&head);
+
+
+    return 0;
+}
+void push(struct node **headRef,int data)
+{
+    struct node *new_node = (struct node *)malloc(sizeof(struct node));
+    new_node->data = data;
+    new_node->next = NULL;
+
+    //if list is empty
+    if((*headRef == NULL))
+    {
+        (*headRef)=new_node;
+        return;
+    }
+    new_node->next=(*headRef);
+    (*headRef)=new_node;
+}
+
+int countList(struct node *headRef)
+{
+    int count =0;
+    while(headRef != NULL)
+    {
+        count++;
+        headRef = headRef->next;
+    }
+    return count;
+}
+
+void deleteList(struct node **headRef)
+{
+    struct node *prev=NULL,*curr=NULL;
+    prev = curr = (*headRef);
+    if(curr == NULL )
+    {
+        printf("list is Empty \n");
+        return;
+    }
+    while(curr != NULL)
+    {
+        prev = curr;
+        curr=curr->next;
+        free(prev);
+    }
+
+    //removing the head reference in main fun
+    free(*headRef);
+}
+void printList(struct node *headRef)
+{
+    while(headRef != NULL )
+    {
+        printf("%d ",headRef->data);
+        headRef=headRef->next;
+    }
+    printf("\n");
+}
